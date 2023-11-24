08f7d4158a5f70c3d38723ec364f47f09cfe1675
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-24 22:10:51 +0530

    Update Card.js
    - modifying image rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,12 @@ import React from "react";
 export default function Card({ title, description, myurl, image }) {
   return (
     <a href={myurl}>
-      <div className="bg-white p-4 rounded-lg shadow-md mb-4">
-        <img className="w-full h-32 object-cover mb-4" src={image} alt="Card" />
+      <div className="bg-white p-4 rounded-lg shadow-md mb-4 max-w-xs mx-auto">
+        <img
+          className="w-full h-40 object-cover mb-4 rounded-md"
+          src={image}
+          alt="Card"
+        />
         <div className="text-xl font-bold mb-2">{title}</div>
         <div className="text-gray-600">{description}</div>
       </div>
