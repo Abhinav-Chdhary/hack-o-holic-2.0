905ec5efa6797a7e2c7f8097301a7c9d34210db7
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-25 03:14:22 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Card({ title, description, myurl, image }) {
   return (
-    <a href={myurl} target="_blank">
-      <div className="bg-white p-4 rounded-lg shadow-md mb-4 max-w-xs mx-auto">
+    <a href={myurl} target="_blank" rel="noopener noreferrer">
+      <div className="bg-white p-4 rounded-lg shadow-md mb-4 max-w-xs mx-auto transition-transform transform hover:scale-105">
         <img
           className="w-full h-40 object-cover mb-4 rounded-md"
           src={image}
